97ed5e0a08c25e2deff68aef52b52b0f28ce7a22
Samantha Gaiser <sgaiser3@gmail.com>
2022-02-08 21:03:01 -0500

    changing css on buttons

---
+++ Untitled-1.js
@@ -0,0 +1,87 @@
+// DOM
+// when you need to change your image as someone clicks a button
+
+// nodes flo chart on github
+// element nodes in green
+// atrribute node ex. class=wrapper 
+// text nodes ex h2
+
+//working w the dom
+//locate nod that res the el you want
+// use its text conte, ch elements
+
+// selecting an element
+    // getElementById() by id
+    // querySelector(. or #) by class gives first result class or id
+        //traverse DOM tree
+
+// Selecting multiple elements
+    // getElementsByClassName() selects all of a classname
+    //querySelectorAll() selects all of one thing indicated?
+
+//Selecting with elements (traversing)
+// parentNode
+// previousSibling/next/Sibling
+// firstChild/lastChild first or last li 
+
+// document.get ElementsByTagName('h1')
+// document.get ElementsByClassName('hot')
+// document.querySelectorAll('li')
+// document.querySelectorAll('li[class="hot"]')
+
+// returning things
+
+//the item() method
+
+//array syntax-- faster than item() syntax
+
+// cont elements = document.getElementsByClassName('hot');
+
+// add elements using DOM
+// createElement()
+// createTextNode() ex an h1 tag
+// appendChild()
+
+// removing elements from the dom
+
+//store element to be removed
+// store parents element
+// removeChild()
+
+//cool fade technique
+// <body class= "blackScreen"
+
+//fade to screen
+//example in dar's github
+
+//give buttons unique id and one class name
+
+// displayObj.src = '';
+
+//add imag to buttons by setting background in css instead of in html
+
+//.button
+// height:;
+// width
+// border: 0;
+// margin
+// cursor:pointer;
+
+// .button:hover
+// border, border-bottom?:; find a hover state
+// transform: rotate(10deg);
+// clicking buttons with one function
+
+// function userPicked(buttonName){
+    // console.log('User Picked ' + buttonName);
+    // swap out large image for button relatedimage 
+    // show the descriptions
+    // play sounds
+// }
+
+// #tony {
+// background-image: src()
+// background-size:cover; if not showing up?
+// }
+
+// background-color:transparent; removes backgrou
